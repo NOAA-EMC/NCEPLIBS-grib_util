451e1560a26d01c9c262de74816650c594e511be
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-12-17 10:51:40 -0700

    more doxygen changes

--- src/tocgrib2super/makwmo.f
+++ src/tocgrib2super/makwmo.f
@@ -1,62 +1,59 @@
-C> @file
-C> FORMAT THE WMO HEADER
-C> @author FARLEY @date 84-07-06
-C
-C> FORMS THE WMO HEADER FOR A GIVEN BULLETIN.
-C>
-C> PROGRAM HISTORY LOG:
-C> -  84-07-06  FARLEY      ORIGINAL AUTHOR
-C> -  94-10-10  R.E.JONES   CHANGES FOR CRAY
-C> -  95-10-18  R.E.JONES   ADD PARAMETER KWBX TO CALL 
-C> -  98-06-16  Gilbert     Changed argument list to pass in day and hour
-C>                         instead of the old O.N. 84 date word.
-C> - 2003-03-28  Gilbert     Removed equivalences.
-C>
-C> USAGE:    CALL MAKWMO(BULHED,IDAY,IHOUR,IMIN,KWBX,HEADER)
-C>   INPUT ARGUMENT LIST:
-C>   @param[in]  BULHED   -  TTAAII BULLETIN HEADER                    FT10
-C>   @param[in]  IDAY     -  Day of Month
-C>   @param[in]   IHOUR    -  Hour of Day.
-C>   @param[in]   IMIN     -  Min of Hour.
-C>   @param[in]   KWBX     -  4 CHARACTERS (KWBC TO KWBQ)
-C>   @param[out] HEADER   -  COMPLETE WMO HEADER IN ASCII
-C>
+!> @file
+!> @brief Format the wmo header.
+!> @author Farley @date 84-07-06
+
+!> Forms the wmo header for a given bulletin.
+!>
+!> ### Program History Log
+!> Date | Programmer | Comments
+!> -----|------------|---------
+!> 84-07-06 | Farley | Original author
+!> 94-10-10 | R. E. Jones | Changes for cray
+!> 95-10-18 | R. E. Jones | Add parameter kwbx to call
+!> 98-06-16 | Gilbert | Changed argument list to pass in day and hour instead of the old O.N. 84 date word.
+!> 2003-03-28 | Gilbert | Removed equivalences.
+!>
+!> @param[in] bulhed ttaaii bulletin header ft10
+!> @param[in] iday day of month
+!> @param[in] ihour hour of day.
+!> @param[in] imin min of hour.
+!> @param[in] kwbx 4 characters (kwbc to kwbq)
+!> @param[out] header complete wmo header in ascii
+!>
+!> @author Farley @date 84-07-06
       SUBROUTINE MAKWMO (BULHED,IDAY,IHOUR,IMIN,KWBX,HEADER)
       CHARACTER * 6 BULHED
       CHARACTER * 1 HEADER (*)
       CHARACTER * 1 WMOHDR (21)
       CHARACTER * 4 KWBX
       CHARACTER * 2 CTEMP
-C
-C--------------------------------------------------------------------
-C
-C$            1.     CREATE WMO HEADER.
-C
-C$            1.1    CONVERT BULHED FROM EBCDIC TO ASCII.
-C
-C     WRITE (6,FMT='('' MADE IT TO MAKWMO'')')
-C
+!
+!$            1.     CREATE WMO HEADER.
+!$            1.1    CONVERT BULHED FROM EBCDIC TO ASCII.
+!
+!     WRITE (6,FMT='('' MADE IT TO MAKWMO'')')
+!
       DO I = 1,6
-        WMOHDR(I) = BULHED(I:I)
+          WMOHDR(I) = BULHED(I:I)
       END DO
-      WMOHDR(7)=char(32)    !  ASCII BLANK
-C
-C     MOVE KWBX INTO WMO HEADER
-C
+      WMOHDR(7)=char(32)        !  ASCII BLANK
+!
+!     MOVE KWBX INTO WMO HEADER
+!
       DO I = 1,4
-        WMOHDR(I+7) = KWBX(I:I)
+          WMOHDR(I+7) = KWBX(I:I)
       END DO
-      WMOHDR(12)=char(32)    !  ASCII BLANK
-C
-C$            1.2    PICK OFF THE DAY OF MONTH (YY)
-C$                   AND CONVERT TO ASCII.
-C
+      WMOHDR(12)=char(32)       !  ASCII BLANK
+!
+!$            1.2    PICK OFF THE DAY OF MONTH (YY)
+!$                   AND CONVERT TO ASCII.
+!
       write(ctemp,fmt='(I2.2)') IDAY
       WMOHDR(13)=ctemp(1:1)
       WMOHDR(14)=ctemp(2:2)
-C
-C$            1.3    PICK OFF THE HOUR(GG) AND CONVERT TO ASCII.
-C
+!
+!$            1.3    PICK OFF THE HOUR(GG) AND CONVERT TO ASCII.
+!
       write(ctemp,fmt='(I2.2)') IHOUR
       WMOHDR(15)=ctemp(1:1)
       WMOHDR(16)=ctemp(2:2)
@@ -65,22 +62,20 @@ C
       WMOHDR(17)=ctemp(1:1)
       WMOHDR(18)=ctemp(2:2)
 
-C
-C             1.4    FIL IN REST OF HEADER
-C
-c     WMOHDR(17)=char(48)    !  ASCII "0"
-c     WMOHDR(18)=char(48)    !  ASCII "0"
-      WMOHDR(19)=char(13)    !  ASCII CR = '\r'
-      WMOHDR(20)=char(13)    !  ASCII CR = '\r'
-      WMOHDR(21)=char(10)    !  ASCII LF = '\n'
-C
-C--------------------------------------------------------------------
-C
-C$            2.     MOVE WMOHDR TO OUTPUT FIELD.
-C
+!
+!             1.4    FIL IN REST OF HEADER
+!
+!     WMOHDR(17)=char(48)    !  ASCII "0"
+!     WMOHDR(18)=char(48)    !  ASCII "0"
+      WMOHDR(19)=char(13)       !  ASCII CR = '\r'
+      WMOHDR(20)=char(13)       !  ASCII CR = '\r'
+      WMOHDR(21)=char(10)       !  ASCII LF = '\n'
+!
+!$            2.     MOVE WMOHDR TO OUTPUT FIELD.
+!
       DO 200 I = 1,21
-        HEADER(I) = WMOHDR(I)
-  200 CONTINUE
-C
+          HEADER(I) = WMOHDR(I)
+ 200  CONTINUE
+!
       RETURN
       END
--- src/tocgrib2super/tocgrib2super.f
+++ src/tocgrib2super/tocgrib2super.f
@@ -1,68 +1,64 @@
-C> @file
-C> @author GILBERT @date 2004-05-17
-C
-C> Program reads selected GRIB2 fields from a file, adds a 
-C>           flag Field separator block and the size in bytes of the grib file,
-C>           and the WMO super header and the time stamp, adds a TOC
-C>           Flag Field separator block and WMO Header in front of each GRIB2
-C>           field, and writes them out to a new file.  The output file
-C>           is in the format required for TOC's FTP Input Service, which
-C>           can be used to disseminate the GRIB2 bulletins.
-C>           This service is described at http://weather.gov/tg/ftpingest.html.
-C>
-C> PROGRAM HISTORY LOG:
-C> -2004-05-17  Gilbert
-C> -2008-02-25  VUONG   Modified the original tocgrib2 program to add super wmo
-C>                     header for NDFD grib file 
-C> -2010-09-02  VUONG   Rename program name aqm_smoke to tocgrib2super
-C> -2012-06-14  VUONG   Modifed write statement with format specifier
-C> -2012-10-22  VUONG   CHANGED VARIABLE ENVVAR TO CHARACTER*6
-C> -2016-10-15  VUONG   Increased length of file name to 200 Characters
-C>
-C> USAGE:
-C>   INPUT FILES:
-C>   -   5       - NAMELIST OF GRIB FIELDS AND ASSOCIATED WMO HEADERS.
-C>   -  11       - INPUT GRIB2 FILE.
-C>   -  12       - Get the file size of the grib file
-C>   -  31       - CORRESPONDING INPUT GRIB2 INDEX FILE.
-C>
-C>   OUTPUT FILES:  (INCLUDING SCRATCH FILES)
-C>   -   6       - STANDARD FORTRAN PRINT FILE
-C>   -  51       - OUTPUT GRIB BULLETIN FILE IN TOC FORMAT
-C>
-C>   SUBPROGRAMS CALLED: (LIST ALL CALLED FROM ANYWHERE IN CODES)
-C>   -  UNIQUE:    - MAKWMO
-C>     LIBRARY:
-C>   -    W3LIB    - W3TAGB W3TAGE mkfldsep makwmo
-C>   -    G2LIB    - GETGB2P
-C>   -    BACIO    - BAREAD BAOPENR BAOPENW BACLOSE WRYTE
-C>
-C>   EXIT STATES:
-C>    - COND =   0 - SUCCESSFUL RUN
-C>    -         10 - Error opening input GRIB2 data file 
-C>    -         20 - Error opening output GRIB transmission file 
-C>    -         19 - ERROR READING CONTROL CARD FILE - All Bulletins missing
-C>    -         30 - Some BULLETINS ARE MISSING
-C>
-C> REMARKS:  The "EXTRACT" variable in the namelist allows users to choose
-C>           whether they want the entire GRIB2 message containing the 
-C>           requested field (extract=.false.), OR a GRIB2 message containing
-C>           only the requested field (extract=.true.).  Both options return the
-C>           same message if the requested field is the only field in the GRIB2
-C>           message.
-C>
+!> @file
+!> @brief Read fields from a GRIB2 file and write them to a new file
+!> with WMO super header.
+!> @author Gilbert @date 2004-05-17
+
+!> Program reads selected GRIB2 fields from a file, adds a flag Field
+!> separator block and the size in bytes of the grib file, and the WMO
+!> super header and the time stamp, adds a TOC Flag Field separator
+!> block and WMO Header in front of each GRIB2 field, and writes them
+!> out to a new file. The output file is in the format required for
+!> TOC's FTP Input Service, which can be used to disseminate the GRIB2
+!> bulletins. This service is described at
+!> http://weather.gov/tg/ftpingest.html.
+!>
+!> @note The "EXTRACT" variable in the namelist allows users to choose
+!> whether they want the entire GRIB2 message containing the requested
+!> field (extract=.false.), OR a GRIB2 message containing only the
+!> requested field (extract=.true.). Both options return the same
+!> message if the requested field is the only field in the GRIB2
+!> message.
+!>
+!> ### Program History Log
+!> Date | Programmer | Comments
+!> -----|------------|---------
+!> 2004-05-17 | Gilbert | Initial
+!> 2008-02-25 | VUONG |  Modified the original tocgrib2 program to add super wmo header for NDFD grib file 
+!> 2010-09-02 | VUONG |  Rename program name aqm_smoke to tocgrib2super
+!> 2012-06-14 | VUONG |  Modifed write statement with format specifier
+!> 2012-10-22 | VUONG |  CHANGED VARIABLE ENVVAR TO CHARACTER*6
+!> 2016-10-15 | VUONG |  Increased length of file name to 200 Characters
+!>
+!> ### Input Files
+!> - 5 Namelist of grib fields and associated wmo headers.
+!> - 11 Input grib2 file.
+!> - 12 Get the file size of the grib file
+!> - 31 Corresponding input grib2 index file.
+!>
+!> ### Output Files  (Including Scratch Files)
+!> - 6 standard fortran print file
+!> - 51 output grib bulletin file in toc format
+!>
+!> @return
+!> -  0 - Successful run
+!> - 10 - Error opening input GRIB2 data file
+!> - 20 - Error opening output GRIB transmission file
+!> - 19 - error reading control card file - all bulletins missing
+!> - 30 - some bulletins are missing
+!>
+!> @author Gilbert @date 2004-05-17
       PROGRAM tocgrib2super
       use grib_mod
       use pdstemplates
       use gridtemplates
-C
+!
       integer,dimension(200) :: IDS,GDT,PDT
       integer   ::    DSCPL,GDTN,PDTN
       integer   ::    nbul,nrec,mbul,dayofmonth,hourofday,minofhour
       integer,parameter :: lenhead=21,jrew=0
       integer    itime(8)
       integer    fsize
-C
+!
       CHARACTER * 16  SUPERWMO
       CHARACTER * 80  DESC,WMOHEAD
       CHARACTER * 200 fileb,filei,fileo,filea
@@ -86,7 +82,7 @@ C
       end interface
 
       NAMELIST /GRIBIDS/DSCPL,IDS,GDTN,GDT,PDTN,PDT,DESC,WMOHEAD,EXTRACT
-C
+!
       CALL W3TAGB('tocgrib2super',2010,0900,0246,'NP11')                  
       lugb=11      ! Input GRIB2 File
       luga=12      ! Input file size
@@ -94,10 +90,10 @@ C
       lugo=51      ! Output transmission file.
       SUPERWMO = 'SUPER WMO HEADER'
       insize = 19  ! The size of the flag field separator
-C
-C        Read GRIB2 data and index file names from the FORTnn
-C        environment variables, and open the files.
-C
+!
+!        Read GRIB2 data and index file names from the FORTnn
+!        environment variables, and open the files.
+!
       envvar='FORT  '
       write(envvar(5:6),fmt='(I2)') lugb
       call getenv(envvar,fileb)
@@ -105,10 +101,10 @@ C
       call getenv(envvar,filea)
       write(envvar(5:6),fmt='(I2)') lugi
       call getenv(envvar,filei)
-C
-C     Get the file size of the grib file
-C
-C     call baopenr(luga,filea,iret1)
+!
+!     Get the file size of the grib file
+!
+!     call baopenr(luga,filea,iret1)
       open(unit=luga,file='filesize',form='formatted',status='old',
      &     iostat=iret1)
       if ( iret1  .ne. 0 ) then
@@ -116,28 +112,28 @@ C     call baopenr(luga,filea,iret1)
          write(6,fmt='(" baopenr error = ",I5)') iret1
          stop 10
       endif
-C
+!
       READ(luga,'(I15)') FSIZE
       print *,' filesize ', fsize
-C
+!
       call baopenr(lugb,fileb,iret1)
       if ( iret1  .ne. 0 ) then
          write(6,fmt='(" Error opening GRIB file: ",A200)')fileb
          write(6,fmt='(" baopenr error = ",I5)') iret1
          stop 10
       endif
-C
-C         Open GRIB2 index file.  If doesn't open, use just the data
-C         file.
-C
+!
+!         Open GRIB2 index file. If doesn't open, use just the data
+!         file.
+!
       call baopenr(lugi,filei,iret2)
       if ( iret2  .ne. 0 ) then
          lugi=0
       endif
-C
-C        Read output GRIB bulletin file name from FORT
-C        environment variable, and open file.
-C
+!
+!        Read output GRIB bulletin file name from FORT
+!        environment variable, and open file.
+!
       envvar='FORT  '
       write(envvar(5:6),fmt='(I2)') lugo
       call getenv(envvar,fileo)
@@ -148,19 +144,19 @@ C
          write(6,fmt='(" baopenw error = ",I5)') iret1
          stop 20
       endif
-c
-c     NDFD grib file begins with a flag flied separator and size bytes of the file
-C     ****nnnnnnnnn****\lf  where "nnnnnnnn" is the size of the file(minus the separator)
-c     The size of the flag field separator is 19 bytes.
-c
+!
+!     NDFD grib file begins with a flag flied separator and size bytes of the file
+!     ****nnnnnnnnn****\lf  where "nnnnnnnn" is the size of the file(minus the separator)
+!     The size of the flag field separator is 19 bytes.
+!
       if (fsize >= insize ) then
          fsize = fsize - insize
       else
          fsize=insize
       ENDIF
-C
-C        loop through input control records.
-C
+!
+!        loop through input control records.
+!
       iret=0
       nbul=0
       nrec = 0
@@ -178,34 +174,34 @@ C
          WMOHEAD='TTAAnn CCCC'
          EXTRACT=.false.
          iopt=2
- 
+
          READ (*,GRIBIDS,iostat=ios,end=999)
-C
+!
          if ( DESC(1:16) .EQ. SUPERWMO(1:16) ) then
-C
-C        MAKE Flag Field Separator block
-C
+!
+!        MAKE Flag Field Separator block
+!
              call mkfldsep(csep,iopt,insize,fsize,lenout)
-C
-C        MAKE SUPER WMO HEADER
-C
-C        GET COMPUTER DATE-TIME & SAVE FOR DATA DATE VERIFICATION
+!
+!        MAKE SUPER WMO HEADER
+!
+!        GET COMPUTER DATE-TIME & SAVE FOR DATA DATE VERIFICATION
              CALL W3UTCDAT(ITIME)
              dayofmonth=ITIME(3)
              hourofday=ITIME(5)
              minofhour=ITIME(6)
              CALL MAKWMO (WMOHEAD(1:6),dayofmonth,hourofday,minofhour,
      &               WMOHEAD(8:11),WMOHDR)
-C
-C        write out Separator block, Abbreviated WMO Heading,
-C        and GRIB2 field to output file.
-C
+!
+!        write out Separator block, Abbreviated WMO Heading,
+!        and GRIB2 field to output file.
+!
              call wryte(lugo,lenout,csep)
              call wryte(lugo,lenhead,WMOHDR)
-C
+!
              write(6,'(16A)') ' DESC=',DESC(1:16)
              write(6,'(11A)') ' WMOHEAD=',WMOHEAD(1:11)
-C
+!
              cycle
          endif
 
@@ -234,9 +230,9 @@ C
             write(6,'(A,I0,A,100(1x,I0))') ' PDT 4. ',PDTN,' =',
      &            (PDT(j2),j2=1,getpdtlen(PDTN))
          endif
-C
-C        Read and return packed GRIB field
-C
+!
+!        Read and return packed GRIB field
+!
          CALL GETGB2P(lugb,lugi,jrew,DSCPL,IDS,PDTN,PDT,
      &               GDTN,GDT,extract,KREW,gribm,itot,iret)
          IF (IRET.NE.0) THEN
@@ -250,23 +246,23 @@ C
          END IF
          WRITE (6,'(A,1x,I0)')' RECORD NO. OF GRIB RECORD IN INPUT '
      &        //'FILE = ', KREW
-C
+!
          WRITE (6,'(A,I0)')' Size of GRIB Field = ',itot
-C
-C        MAKE Flag Field Separator block
-C
+!
+!        MAKE Flag Field Separator block
+!
           call mkfldsep(csep,iopt,insize,itot+lenhead,lenout)
-C         WRITE(6,'(A,80A)')'  csep = ',csep
-C
-C        MAKE WMO HEADER
-C
+!         WRITE(6,'(A,80A)')'  csep = ',csep
+!
+!        MAKE WMO HEADER
+!
          CALL MAKWMO (WMOHEAD(1:6),dayofmonth,hourofday,minofhour,
      &               WMOHEAD(8:11),WMOHDR)
-C         WRITE(6,'(21A)') '  WMOHEADER= ',WMOHDR
-C
-C        write out Separator block, Abbreviated WMO Heading,
-C        and GRIB2 field to output file.
-C
+!         WRITE(6,'(21A)') '  WMOHEADER= ',WMOHDR
+!
+!        write out Separator block, Abbreviated WMO Heading,
+!        and GRIB2 field to output file.
+!
          call wryte(lugo,lenout,csep)
          call wryte(lugo,lenhead,WMOHDR)
          call wryte(lugo,itot,gribm)
@@ -275,16 +271,16 @@ C
             deallocate(gribm)
             nullify(gribm)
          endif
-C
+!
       enddo foreachinputrecord
-C
-C     CLOSING SECTION
-C
+!
+!     CLOSING SECTION
+!
  999  if (nbul .EQ. 0 ) then
         WRITE (6,FMT='('' SOMETHING WRONG WITH DATA CARDS...'',
      &         ''NOTHING WAS PROCESSED'')')
-c        CALL W3TAGE('tocgrib2super')                                       
-        stop 19 
+!        CALL W3TAGE('tocgrib2super')                                       
+        stop 19
       else
         call baclose (LUGB,iret)
         call baclose (LUGI,iret)
@@ -294,16 +290,16 @@ c        CALL W3TAGE('tocgrib2super')
      &    /,5X,''WROTE '',I6,'' BULLETINS OUT FOR TRANSMISSION'',
      &    //)') nrec, NBUL
       endif
-C
-C         TEST TO SEE IF ANY BULLETINS MISSING
-C
+!
+!         TEST TO SEE IF ANY BULLETINS MISSING
+!
       mbul = nrec - nbul
       IF (mbul .ne. 0) THEN
         WRITE(6,'(A,1X,I0)')' BULLETINS MISSING = ',mbul
-c        CALL W3TAGE('tocgrib2super')                                     
+!        CALL W3TAGE('tocgrib2super')                                     
         stop 30
       END IF
-C
-c      CALL W3TAGE('tocgrib2super')                                       
+!
+!      CALL W3TAGE('tocgrib2super')                                       
       STOP
       END
