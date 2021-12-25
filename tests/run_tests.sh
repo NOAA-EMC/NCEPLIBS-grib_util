b8c89b921e47707a75fcb9c5d0ccbe6e2eb12205
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-12-25 05:39:13 -0700

    adding test

---
+++ tests/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a test script for the NCEPLIBS-grib_util project.
+#
+# Ed Hartnett, 12/25/21
+
+set -e
+echo ""
+
+
+echo "*** Running group_rename test"
+
+FAIL=0
+
+exit $FAIL
