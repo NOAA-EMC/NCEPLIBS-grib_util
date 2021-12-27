86c4f8a650ca50d933f4695f3f28a11406559bf7
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-12-27 06:25:51 -0700

    adding test

---
+++ tests/run_tests_2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# This is a test script for the NCEPLIBS-grib_util project.
+#
+# Ed Hartnett, 12/27/21
+
+set -e
+echo ""
+echo "*** Running copygb2 test"
+
+# Copy GRIB2 file.
+../src/copygb2/copygb2 -x gdaswave.t00z.wcoast.0p16.f000.grib2 gdaswave_2.grib2
+
+# Create an index of the copied file.
+../src/grb2index/grb2index gdaswave_2.grib2 gdaswave_2.idx
+
+# Check against expected output. First 120 bytes contain differences,
+# so ignore them.
+cmp -i 120 gdaswave_2.idx ref_gdaswave_2.idx
+
+echo "*** SUCCESS!"
+exit 0
