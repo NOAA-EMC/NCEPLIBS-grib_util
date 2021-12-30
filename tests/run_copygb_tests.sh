3bf89f3e0f00249909de59bc7a20f244d4128c60
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-12-30 06:13:57 -0700

    added copygb test

--- tests/run_copygb_tests.sh
+++ tests/run_copygb_tests.sh
@@ -8,14 +8,14 @@ echo ""
 echo "*** Running copygb test"
 
 # Copy GRIB2 file.
-#../src/copygb2/copygb2 -x gdaswave.t00z.wcoast.0p16.f000.grib2 gdaswave_2.grib2
+../src/copygb/copygb -x ref_gdaswave.t00z.wcoast.0p16.f000.grib1 test_gdaswave_2.grib1
 
 # Create an index of the copied file.
-#../src/grb2index/grb2index gdaswave_2.grib2 gdaswave_2.idx
+../src/grbindex/grbindex test_gdaswave_2.grib1 test_gdaswave_2.grib1.idx
 
 # Check against expected output. First 120 bytes contain differences,
 # so ignore them.
-#cmp -i 120 gdaswave_2.idx ref_gdaswave_2.idx
+cmp -i 120 test_gdaswave_2.grib1.idx ref_gdaswave_2.grib1.idx
 
 echo "*** SUCCESS!"
 exit 0
