9abb89c285e3045d7000671774460dfb622919c3
Edward Hartnett <Edward.Hartnett@noaa.gov>
2022-01-03 07:05:15 -0700

    whitespace cleanup of cnvgrib.F90

--- src/cnvgrib/cnvgrib.F90
+++ src/cnvgrib/cnvgrib.F90
@@ -28,29 +28,29 @@
 !> @author Stephen Gilbert @date 2003-06-06
 program cnvgrib
 
-  integer :: inver=0,outver=0,ipack=-1
-  character(len=500) :: gfilein,gfileout,copt
-  character(len=2) :: master_table_ver,curmastertab_ver
-  INTEGER(4) NARG,IARGC, table_ver, mastertab
-  logical :: usemiss=.false., uvvect=.true.
+  integer :: inver = 0, outver = 0, ipack = -1
+  character(len = 500) :: gfilein, gfileout, copt
+  character(len = 2) :: master_table_ver, curmastertab_ver
+  INTEGER(4) NARG, IARGC, table_ver, mastertab
+  logical :: usemiss = .false., uvvect = .true.
   !
   !     Set current Master table version 2
   !
-  curmastertab_ver='2'
-  table_ver=2
-  mastertab=21    ! WMO GRIB2 version 21 (released in May 2, 2018)
+  curmastertab_ver = '2'
+  table_ver = 2
+  mastertab = 21    ! WMO GRIB2 version 21 (released in May 2, 2018)
 
   !  GET ARGUMENTS
-  NARG=IARGC()
+  NARG = IARGC()
   IF(NARG.lt.3) THEN       ! may be a problem with args
-     IF(NARG.eq.0) THEN
+     IF(NARG .eq. 0) THEN
         !CALL ERRMSG('cnvgrib:  Incorrect usage')
         call usage(0)
         CALL ERREXIT(2)
      ELSE                !  look for -h "help" option
-        do j=1,NARG
-           call getarg(j,copt)
-           if (copt.eq.'-h' .or. copt.eq.'-help') then
+        do j = 1, NARG
+           call getarg(j, copt)
+           if (copt .eq. '-h' .or. copt .eq. '-help') then
               call usage(1)
               CALL ERREXIT(0)
            endif
@@ -59,110 +59,110 @@ program cnvgrib
         CALL ERREXIT(2)
      ENDIF
   ELSE
-     j=1
+     j = 1
      do while (j.le.NARG-2)        ! parse first narg-2 args
-     call getarg(j,copt)
-     j=j+1
+     call getarg(j, copt)
+     j = j+1
      selectcase(copt)
      case('-g12')
-        inver=1
-        outver=2
+        inver = 1
+        outver = 2
      case('-g21')
-        inver=2
-        outver=1
+        inver = 2
+        outver = 1
      case('-g22')
-        inver=2
-        outver=2
+        inver = 2
+        outver = 2
      case('-p0')
-        ipack=0
+        ipack = 0
      case('-p2')
-        ipack=2
+        ipack = 2
      case('-p31')
-        ipack=31
+        ipack = 31
      case('-p32')
-        ipack=32
+        ipack = 32
      case('-p40')
-        ipack=40
+        ipack = 40
      case('-p41')
-        ipack=41
+        ipack = 41
      case('-p40000')       ! Obsolete
-        ipack=40000
+        ipack = 40000
      case('-p40010')       ! Obsolete
-        ipack=40010
+        ipack = 40010
      case('-m')
-        usemiss=.true.
-        imiss=1
+        usemiss = .true.
+        imiss = 1
      case('-m0')
-        usemiss=.true.
-        imiss=0
+        usemiss = .true.
+        imiss = 0
      case('-nv')
-        uvvect=.false.
+        uvvect = .false.
      case('-mastertable_ver_1')
-        table_ver=1
-        master_table_ver='1'
+        table_ver = 1
+        master_table_ver = '1'
      case('-mastertable_ver_2')
-        table_ver=2
-        master_table_ver='2'
+        table_ver = 2
+        master_table_ver = '2'
      case('-mastertable_ver_3')
-        table_ver=3
-        master_table_ver='3'
+        table_ver = 3
+        master_table_ver = '3'
      case('-mastertable_ver_4')
-        table_ver=4
-        master_table_ver='4'
+        table_ver = 4
+        master_table_ver = '4'
      case('-mastertable_ver_5')
-        table_ver=5
-        master_table_ver='5'
+        table_ver = 5
+        master_table_ver = '5'
      case('-mastertable_ver_6')
-        table_ver=6
-        master_table_ver='6'
+        table_ver = 6
+        master_table_ver = '6'
      case('-mastertable_ver_7')
-        table_ver=7
-        master_table_ver='7'
+        table_ver = 7
+        master_table_ver = '7'
      case('-mastertable_ver_8')
-        table_ver=8
-        master_table_ver='8'
+        table_ver = 8
+        master_table_ver = '8'
      case('-mastertable_ver_9')
-        table_ver=9
-        master_table_ver='9'
+        table_ver = 9
+        master_table_ver = '9'
      case('-mastertable_ver_10')
-        table_ver=10
-        master_table_ver='10'
+        table_ver = 10
+        master_table_ver = '10'
      case('-mastertable_ver_11')
-        table_ver=11
-        master_table_ver='11'
+        table_ver = 11
+        master_table_ver = '11'
      case('-mastertable_ver_12')
-        table_ver=12
-        master_table_ver='12'
+        table_ver = 12
+        master_table_ver = '12'
      case('-mastertable_ver_13')
-        table_ver=13
-        master_table_ver='13'
+        table_ver = 13
+        master_table_ver = '13'
      case('-mastertable_ver_14')
-        table_ver=14
-        master_table_ver='14'
+        table_ver = 14
+        master_table_ver = '14'
      case('-mastertable_ver_15')
-        table_ver=15
-        master_table_ver='15'
+        table_ver = 15
+        master_table_ver = '15'
      case('-mastertable_ver_16')
-        table_ver=16
-        master_table_ver='16'
+        table_ver = 16
+        master_table_ver = '16'
      case('-mastertable_ver_17')
-        table_ver=17
-        master_table_ver='17'
+        table_ver = 17
+        master_table_ver = '17'
      case('-mastertable_ver_18')
-        table_ver=18
-        master_table_ver='18'
+        table_ver = 18
+        master_table_ver = '18'
      case('-mastertable_ver_19')
-        table_ver=19
-        master_table_ver='19'
+        table_ver = 19
+        master_table_ver = '19'
      case('-mastertable_ver_20')
-        table_ver=20
-        master_table_ver='20'
+        table_ver = 20
+        master_table_ver = '20'
      case('-mastertable_ver_21')
-        table_ver=21
-        master_table_ver='21'
+        table_ver = 21
+        master_table_ver = '21'
      case('-mastertable_ver_22')
-        table_ver=22
-        master_table_ver='22'
+        table_ver = 22
+        master_table_ver = '22'
      case default
         call usage(0)
         CALL ERREXIT(2)
@@ -184,18 +184,18 @@ program cnvgrib
   !
   !   get filenames from last two arguments
   !
-  CALL GETARG(NARG-1,gfilein)
-  CALL GETARG(NARG,gfileout)
+  CALL GETARG(NARG-1, gfilein)
+  CALL GETARG(NARG, gfileout)
   !
   !   If -p option specified, must be writing out grib2
   !
-  if ((ipack.ne.-1).and.(outver.eq.1)) then
+  if ((ipack .ne. -1).and.(outver .eq. 1)) then
      CALL ERRMSG('cnvgrib: -pxx option ignored when using -g21')
   endif
   !
   !   Must have -g option
   !
-  if ((inver.eq.0).or.(outver.eq.0)) then
+  if ((inver .eq. 0).or.(outver .eq. 0)) then
      CALL ERRMSG('cnvgrib: must use one -gxx option')
      call usage(0)
      CALL ERREXIT(2)
@@ -204,28 +204,28 @@ program cnvgrib
   !   If -m or -m0 option specified, must be writing out grib2
   !   and using DRT 5.2 or 5.3
   !
-  if ((usemiss).and.(ipack.ne.2 .AND. ipack.ne.31 .AND. &
-       ipack.ne.32)) then
+  if ((usemiss).and.(ipack .ne. 2 .AND. ipack .ne. 31 .AND. &
+       ipack .ne. 32)) then
      CALL ERRMSG('cnvgrib: -m or -m0 option ignored when not '// &
           'using -p2, -p31 or -p32.')
-     usemiss=.false.
+     usemiss = .false.
   endif
 ENDIF
 !
 !  Open input and output grib files
 !
-IFL1=10
-IFL2=50
-NCGB=LEN_TRIM(gfilein)
-CALL BAOPENR(ifl1,gfilein(1:NCGB),IOS)
-if (IOS.NE.0) then
+IFL1 = 10
+IFL2 = 50
+NCGB = LEN_TRIM(gfilein)
+CALL BAOPENR(ifl1, gfilein(1:NCGB), IOS)
+if (IOS .NE. 0) then
   call errmsg('cnvgrib: cannot open input GRIB file '// &
        gfilein(1:NCGB))
   call errexit(3)
 endif
-NCGB=LEN_TRIM(gfileout)
-CALL BAOPENW(ifl2,gfileout(1:NCGB),IOS)
-if (IOS.NE.0) then
+NCGB = LEN_TRIM(gfileout)
+CALL BAOPENW(ifl2, gfileout(1:NCGB), IOS)
+if (IOS .NE. 0) then
   call errmsg('cnvgrib: cannot open output GRIB file '// &
        gfileout(1:NCGB))
   call errexit(4)
@@ -233,21 +233,21 @@ endif
 !
 !  convert grib file
 !
-if ((inver.eq.1).AND.(outver.eq.2)) then
-  call cnv12(ifl1,ifl2,ipack,usemiss,imiss,uvvect, table_ver)
-elseif ((inver.eq.2).AND.(outver.eq.1)) then
-  call cnv21(ifl1,ifl2)
-elseif ((inver.eq.2).AND.(outver.eq.2)) then
-  call cnv22(ifl1,ifl2,ipack,usemiss,imiss,table_ver)
+if ((inver .eq. 1).AND.(outver .eq. 2)) then
+  call cnv12(ifl1, ifl2, ipack, usemiss, imiss, uvvect, table_ver)
+elseif ((inver .eq. 2).AND.(outver .eq. 1)) then
+  call cnv21(ifl1, ifl2)
+elseif ((inver .eq. 2).AND.(outver .eq. 2)) then
+  call cnv22(ifl1, ifl2, ipack, usemiss, imiss, table_ver)
 else
-  print *,' Unknown conversion option.'
+  print *, ' Unknown conversion option.'
   call errexit(5)
 endif
 !
 !  close grib files
 !
-CALL BACLOSE(ifl1,IOS)
-CALL BACLOSE(ifl2,IOS)
+CALL BACLOSE(ifl1, IOS)
+CALL BACLOSE(ifl2, IOS)
 
 stop
 end program
@@ -259,10 +259,10 @@ end program
 !>
 !> @author Stephen Gilbert @date 2003-06-06
 subroutine usage(iopt)
-character(len=15) :: cnvgrib_ver="cnvgrib-v3.1.1"
-integer,intent(in) :: iopt
+character(len = 15) :: cnvgrib_ver = "cnvgrib-v3.1.1"
+integer, intent(in) :: iopt
 
-if (iopt.eq.0) then
+if (iopt .eq. 0) then
   call errmsg ('  ')
   call errmsg('Usage: cnvgrib [-h] {-g12|-g21|-g22} [-m|-m0]'// &
        ' [-nv] [-mastertable_ver_x]')
@@ -274,7 +274,7 @@ if (iopt.eq.0) then
   call errmsg ('  ')
 endif
 
-if (iopt.eq.1) then
+if (iopt .eq. 1) then
   call errmsg ('  ')
   call errmsg('cnvgrib:  version '//cnvgrib_ver)
   call errmsg ('  ')
