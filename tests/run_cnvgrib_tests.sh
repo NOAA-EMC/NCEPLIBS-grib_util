75444a616cb82365c174814c778cefe694b658dc
Edward Hartnett <Edward.Hartnett@noaa.gov>
2022-01-03 06:47:43 -0700

    more testing

--- tests/run_cnvgrib_tests.sh
+++ tests/run_cnvgrib_tests.sh
@@ -19,6 +19,12 @@ cmp test_gdaswave.t00z.wcoast.0p16.f000.grib1.inventory.txt ref_gdaswave_grib1_i
 # Convert GRIB1 output back to GRIB2.
 ../src/cnvgrib/cnvgrib -g12 test_gdaswave.t00z.wcoast.0p16.f000.grib1 test_gdaswave.t00z.wcoast.0p16.f000.grib2
 
+# Create an index of a GRIB2 file.
+../src/grb2index/grb2index test_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000.grib2.idx
+
+# Check against expected output. First 120 bytes contain differences,
+# so ignore them.
+cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000.grib2.idx ref_gdaswave.t00z.wcoast.0p16.f000.grib2.idx
 
 echo "*** SUCCESS!"
 exit 0
