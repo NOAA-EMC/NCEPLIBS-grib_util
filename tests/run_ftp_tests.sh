3f4a8ddcb7478c980106b771664fd6867e0aff13
Edward Hartnett <edward.hartnett@noaa.gov>
2022-08-08 06:40:17 -0600

    more testing with FTP file

--- tests/run_ftp_tests.sh
+++ tests/run_ftp_tests.sh
@@ -16,17 +16,17 @@ echo "*** Running FTP file tests"
 ../src/wgrib/wgrib test_WW3_West.grib1 &> test_WW3_West.grib1.inventory.txt
 
 # Check against expected output.
-#cmp test_gdaswave.t00z.wcoast.0p16.f000.grib1.inventory.txt ref_gdaswave_grib1_inventory.txt
+cmp test_gdaswave.t00z.wcoast.0p16.f000.grib1.inventory.txt ref_gdaswave_grib1_inventory.txt
 
 # Convert GRIB1 output back to GRIB2.
-#../src/cnvgrib/cnvgrib -g12 test_gdaswave.t00z.wcoast.0p16.f000.grib1 test_gdaswave.t00z.wcoast.0p16.f000.grib2
+../src/cnvgrib/cnvgrib -g12 test_WW3_West.grib1 test_WW3_West.grib2
 
 # Create an index of a GRIB2 file.
-#../src/grb2index/grb2index test_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000.grib2.idx
+../src/grb2index/grb2index test_WW3_West.grib2 test_WW3_West.grib2.idx
 
 # Check against expected output. First 120 bytes contain differences,
 # so ignore them.
-#cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000.grib2.idx ref_gdaswave.t00z.wcoast.0p16.f000.grib2.idx
+cmp -i 120 test_WW3_West.grib2.idx ref_test_WW3_West.grib2.idx
 
 # Convert test file to another GRIB2 file.
 #../src/cnvgrib/cnvgrib -g22 ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_2.grib2
