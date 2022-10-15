00b3ed003d4516d8f6482d39dda98b5ca9fcd0a7
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-15 16:16:32 +0200

    fixed index documentation

--- src/grb2index/grb2index.F90
+++ src/grb2index/grb2index.F90
@@ -22,21 +22,23 @@
 !> Each record in the index table contains the following fields. All
 !> integers are in big-endian format in the file.
 !>
-!> -  byte 001 - 004: length of index record
-!> -  byte 005 - 008: bytes to skip in data file before GRIB message
-!> -  byte 009 - 012: bytes to skip in message before gds
-!> -  byte 013 - 016: bytes to skip in message before pds
-!> -  byte 017 - 020: bytes to skip in message before drs
-!> -  byte 021 - 024: bytes to skip in message before bms
-!> -  byte 025 - 032: bytes total in the message
-!> -  byte 033 - 033: GRIB version number ( currently 2 )
-!> -  byte 034 - 034: message discipline
-!> -  byte 035 - 036: field number within GRIB2 message
-!> -  byte 037 -  ii: identification section (ids)
-!> -  byte ii+1-  jj: grid definition section (gds)
-!> -  byte jj+1-  kk: product definition section (pds)
-!> -  byte kk+1-  ll: the data representation section (drs)
-!> -  byte ll+1-ll+6: first 6 bytes of the bit map section (bms)
+!> - byte 001 - 004 length of index record
+!> - byte 005 - 008 bytes to skip in data file before grib message
+!> - byte 009 - 012 bytes to skip in message before lus (local use) set = 0, if no local section.
+!> - byte 013 - 016 bytes to skip in message before gds
+!> - byte 017 - 020 bytes to skip in message before pds
+!> - byte 021 - 024 bytes to skip in message before drs
+!> - byte 025 - 028 bytes to skip in message before bms
+!> - byte 029 - 032 bytes to skip in message before data section
+!> - byte 033 - 040 bytes total in the message
+!> - byte 041 - 041 grib version number (currently 2)
+!> - byte 042 - 042 message discipline
+!> - byte 043 - 044 field number within grib2 message
+!> - byte 045 -  ii identification section (ids)
+!> - byte ii+1-  jj grid definition section (gds)
+!> - byte jj+1-  kk product definition section (pds)
+!> - byte kk+1-  ll the data representation section (drs)
+!> - byte ll+1-ll+6 first 6 bytes of the bit map section (bms)
 !>
 !> ### Program History Log
 !> Date | Programmer | Comments
