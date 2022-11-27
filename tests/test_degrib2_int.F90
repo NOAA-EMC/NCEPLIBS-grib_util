aaa98751e7590988f53c2a0c7660c233d10bef64
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-27 08:01:51 -0700

    adding date/time description testing

--- tests/test_degrib2_int.F90
+++ tests/test_degrib2_int.F90
@@ -8,20 +8,27 @@ program test_degrib2_int
   integer :: pt_0_0(15) = (/ 2, 1, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0 /)
   integer :: pt_0_1(15) = (/ 2, 10, 0, 0, 81, 0, 0, 1, 0, 100, 0, 80000, 255, 0, 0 /)
   integer :: pt_0_2(15) = (/ 0, 21, 2, 255, 104, 65535, 255, 1, 1, 103, 0, 2, 255, 0, 0 /)
+  integer :: s1_0(13) = (/ 7, 14, 1, 1, 1, 2022, 11, 17, 19, 0, 0, 0, 1 /)
   character(len = 40) :: la
+  character(len = 100) :: ta
 
-  print *, 'Testing degrib2 level descriptions...'
+  print *, 'Testing degrib2 level and date/time descriptions...'
 
   ! Template 0 with various options.
   call prlevel(0, pt_0_0, la)
   if (la .ne. " Surface") stop 10
+  call prvtime(0, pt_0_0, s1_0, ta)
+  if (trim(ta) .ne.  "valid  0 hour after 2022111719:00:00") stop 11
 
   call prlevel(0, pt_0_1, la)
-  if (la .ne. " 800 mb") stop 11
-
+  if (la .ne. " 800 mb") stop 21
+  call prvtime(0, pt_0_1, s1_0, ta)
+  if (trim(ta) .ne.  "valid  0 hour after 2022111719:00:00") stop 22
+  
   call prlevel(0, pt_0_2, la)
-  print *, '!', la, '!'
-  if (trim(la) .ne. "2 m above ground") stop 12
+  if (trim(la) .ne. "2 m above ground") stop 31
+  call prvtime(0, pt_0_2, s1_0, ta)
+  if (trim(ta) .ne.  "valid  1 hour after 2022111719:00:00") stop 32
 
   print *, 'SUCCESS!'
   
