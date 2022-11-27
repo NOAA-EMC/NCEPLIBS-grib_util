2794c358eeb58fd2b781da2937d63bc57e71d2ca
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-27 06:55:27 -0700

    adding test for degrib2 level descriptions

---
+++ tests/test_degrib2_level.F90
@@ -0,0 +1,24 @@
+! This is a test program for NCEPLIBS-grib_util.
+!
+! This program tests the level description in the degrib2 utility.
+!
+! Ed Hartnett 11/27/22
+program test_degrib2_level
+  implicit none
+  integer :: pt_0_0(15) = (/ 2, 1, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0 /)
+  integer :: pt_0_1(15) = (/ 2, 10, 0, 0, 81, 0, 0, 1, 0, 100, 0, 80000, 255, 0, 0 /)
+  character(len = 40) :: la
+
+  print *, 'Testing degrib2 level descriptions...'
+
+  ! Template 0 with various options.
+  call prlevel(0, pt_0_0, la)
+  if (la .ne. " Surface") stop 10
+
+  call prlevel(0, pt_0_1, la)
+  print *, la
+  if (la .ne. " 800 mb") stop 10
+
+  print *, 'SUCCESS!'
+  
+end program test_degrib2_level
