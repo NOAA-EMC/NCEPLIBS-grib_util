d21990db9d02156ac2759545855ad1782a133684
George.Gayno <george.gayno@noaa.gov>
2023-01-13 16:22:51 +0000

    Add copygb test for NCEP grids 172 and 220.
    Fixes #202.

--- tests/run_copygb_tests.sh
+++ tests/run_copygb_tests.sh
@@ -7,7 +7,7 @@ set -e
 echo ""
 echo "*** Running copygb test"
 
-# Copy GRIB2 file.
+# Copy GRIB1 file.
 ../src/copygb/copygb -x data/ref_gdaswave.t00z.wcoast.0p16.f000.grib1 test_gdaswave_2.grib1
 
 # Create an index of the copied file.
@@ -16,6 +16,14 @@ echo "*** Running copygb test"
 # Check against expected output. First 120 bytes contain differences,
 # so ignore them.
 cmp -i 120 test_gdaswave_2.grib1.idx data/ref_gdaswave_2.grib1.idx
+
+# Interpolate GFS landmask to NCEP grid 172
+../src/copygb/copygb -g172 -x data/ref_gfs.landmask.grib1 172.land.grib1
+cmp 172.land.grib1 data/ref_grid_172.landmask.grib1
+
+# Interpolate GFS landmask to NCEP grid 220
+../src/copygb/copygb -g220 -x data/ref_gfs.landmask.grib1 220.land.grib1
+cmp 220.land.grib1 data/ref_grid_220.landmask.grib1
 
 echo "*** SUCCESS!"
 exit 0
