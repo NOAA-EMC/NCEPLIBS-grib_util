a0f05cb32f9eaea998af082c7e6758b08f114f1b
Edward Hartnett <edward.hartnett@noaa.gov>
2023-01-25 14:17:12 +0100

    trying to fix CI

--- tests/run_copygb2_tests.sh
+++ tests/run_copygb2_tests.sh
@@ -13,12 +13,5 @@ echo "*** Running copygb2 test"
 # Are the files the same?
 g2c_compare -v data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave_2.grib2
 
-# Create an index of the copied file.
-#../src/grb2index/grb2index test_gdaswave_2.grib2 test_gdaswave_2.idx
-
-# Check against expected output. First 120 bytes contain differences,
-# so ignore them.
-#cmp -i 120 test_gdaswave_2.idx data/ref_gdaswave_2.idx
-
 echo "*** SUCCESS!"
 exit 0
