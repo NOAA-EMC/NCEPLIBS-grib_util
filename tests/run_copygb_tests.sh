ab6de422f9555349a4315e165cf9031d5a619777
Edward Hartnett <edward.hartnett@noaa.gov>
2023-06-24 08:41:00 -0600

    turned tests back on

--- tests/run_copygb_tests.sh
+++ tests/run_copygb_tests.sh
@@ -18,12 +18,12 @@ echo "*** Running copygb test"
 cmp -i 120 test_gdaswave_2.grib1.idx data/ref_gdaswave_2.grib1.idx
 
 # Interpolate GFS landmask to NCEP grid 172
-#../src/copygb/copygb -g172 -x data/ref_gfs.landmask.grib1 172.land.grib1
-#cmp 172.land.grib1 data/ref_grid_172.landmask.grib1
+../src/copygb/copygb -g172 -x data/ref_gfs.landmask.grib1 172.land.grib1
+cmp 172.land.grib1 data/ref_grid_172.landmask.grib1
 
 # Interpolate GFS landmask to NCEP grid 220
-#../src/copygb/copygb -g220 -x data/ref_gfs.landmask.grib1 220.land.grib1
-#cmp 220.land.grib1 data/ref_grid_220.landmask.grib1
+../src/copygb/copygb -g220 -x data/ref_gfs.landmask.grib1 220.land.grib1
+cmp 220.land.grib1 data/ref_grid_220.landmask.grib1
 
 echo "*** SUCCESS!"
 exit 0
