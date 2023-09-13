50d44fb67cb1da665a93bd37acbbefbd51349477
Edward Hartnett <edward.hartnett@noaa.gov>
2023-09-13 04:01:24 -0600

    more test verbosity

--- tests/run_grb2index_tests.sh
+++ tests/run_grb2index_tests.sh
@@ -4,36 +4,36 @@
 # Ed Hartnett, 12/27/21
 
 set -e
-set -x
 echo ""
 echo "*** Running grb2index tests"
 
 # Confirm that 2 arguments are required, otherwise an error code of 2
 # is returned.
 echo "*** expect failure..."
+set -x
 ../src/grb2index/grb2index && exit 1
 [ $? -ne 2 ] && exit 2
-echo "*** expect failure..."
 ../src/grb2index/grb2index one && exit 1
 [ $? -ne 2 ] && exit 2
 
 # Confirm that input file can be read, otherwise an error code of 8 is returned.
-echo "*** expect failure..."
 ../src/grb2index/grb2index missing1 missing2 && exit 1
 [ $? -ne 8 ] && exit 2
 
 # When there are no grib2 messages in the file, an error code of 1 is returned.
-echo "*** expect failure..."
 ../src/grb2index/grb2index data/ref_gdaswave_2.idx missing2 && exit 1
 [ $? -ne 1 ] && exit 2
+set +x
 
 # Create an index of a GRIB2 file.
 echo "*** expect success..."
+set -x
 ../src/grb2index/grb2index data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.grb2index.idx
 
 # Check against expected output. First 120 bytes contain differences,
 # so ignore them.
 cmp -i 140 test_gdaswave.grb2index.idx data/ref_gdaswave.grb2index.idx
+set +x
 
 echo "*** SUCCESS!"
 exit 0
