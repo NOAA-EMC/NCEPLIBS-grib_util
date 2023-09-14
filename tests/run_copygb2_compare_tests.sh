0230cc988226a532d6e62bbf34f57a13c792df5d
Edward Hartnett <edward.hartnett@noaa.gov>
2023-09-14 00:43:25 -0600

    better testing for copygb2

--- tests/run_copygb2_compare_tests.sh
+++ tests/run_copygb2_compare_tests.sh
@@ -10,8 +10,14 @@ echo "*** Running copygb2 test"
 # Copy GRIB2 file.
 ../src/copygb2/copygb2 -x data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave_2.grib2
 
+# Are the files the same?
+g2c_compare -v data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave_2.grib2
+
 # Invoke interpolation logic.
 ../src/copygb2/copygb2 -g "30 6 0 0 0 0 0 0 1473 1025 12190000 226541000 8 25000000 265000000 5079000 5079000 0 64 25000000 25000000" -i"1 1" -x data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave_2.ip.grib2
+
+# Are the files the same?
+g2c_compare -v data/ref_gdaswave.t00z.wcoast.0p16.f000.ip.grib2 test_gdaswave_2.ip.grib2
 
 echo "*** SUCCESS!"
 exit 0
