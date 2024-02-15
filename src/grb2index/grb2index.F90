d3ff685f63d13d2f53b31c7b5641d2f69fe77bd4
Edward Hartnett <edward.hartnett@noaa.gov>
2024-02-14 17:39:08 -0700

    clean up

--- src/grb2index/grb2index.F90
+++ src/grb2index/grb2index.F90
@@ -12,11 +12,15 @@
 !>
 !> @author Iredell @date 1992-11-22
 program grb2index
-  parameter(msk1=32000,msk2=4000)
-  character cgb*256,cgi*256
-  character(len=1),pointer,dimension(:) :: cbuf
-  character carg*300
-  integer narg,iargc
+  implicit none
+  integer msk1, msk2
+  parameter(msk1 = 32000, msk2 = 4000)
+  character cgb * 256, cgi * 256
+  character(len = 1),pointer,dimension(:) :: cbuf
+  character carg * 300
+  integer narg, iargc
+  integer ios, iret, irgi, iw, lcarg, mnum, ncgb, ncgi
+  integer :: numtot, nnum, nmess, nlen, kw
   interface
      subroutine getg2ir(lugb,msk1,msk2,mnum,cbuf,nlen,nnum, &
           nmess,iret)
@@ -107,50 +111,50 @@ end program grb2index
 !> @param[in] cgb character name of GRIB file
 !>
 !> @author Iredell @date 93-11-22
-subroutine wrgi1h(lugi,nlen,nnum,cgb)
+subroutine wrgi1h(lugi, nlen, nnum, cgb)
+  implicit none
   character cgb*(*)
+  character cd8*8, ct10*10, hostname*15
 #ifdef __GFORTRAN__
-  character cd8*8,ct10*10,hostname*15
   integer istat
-#else
-  character cd8*8,ct10*10,hostnam*15
 #endif
   character chead(2)*81
+  integer lugi, nlen, nnum, kw, ncgb, ncgb1, ncgb2, ncbase
 
   !  fill first 81-byte header
-  ncgb=len(cgb)
-  ncgb1=ncbase(cgb,ncgb)
-  ncgb2=ncbase(cgb,ncgb1-2)
-  call date_and_time(cd8,ct10)
-  chead(1)='!GFHDR!'
-  chead(1)(9:10)=' 1'
-  chead(1)(12:14)='  1'
+  ncgb = len(cgb)
+  ncgb1 = ncbase(cgb, ncgb)
+  ncgb2 = ncbase(cgb, ncgb1 - 2)
+  call date_and_time(cd8, ct10)
+  chead(1) = '!GFHDR!'
+  chead(1)(9:10) = ' 1'
+  chead(1)(12:14) = '  1'
   write(chead(1)(16:20),'(i5)') 162
-  chead(1)(22:31)=cd8(1:4)//'-'//cd8(5:6)//'-'//cd8(7:8)
-  chead(1)(33:40)=ct10(1:2)//':'//ct10(3:4)//':'//ct10(5:6)
-  chead(1)(42:47)='GB2IX1'
-  chead(1)(49:54)='      '
+  chead(1)(22:31) = cd8(1:4) // '-' // cd8(5:6) // '-' // cd8(7:8)
+  chead(1)(33:40) = ct10(1:2) // ':' // ct10(3:4) // ':' //ct10(5:6)
+  chead(1)(42:47) = 'GB2IX1'
+  chead(1)(49:54) = '      '
 #ifdef __GFORTRAN__
   istat=hostnm(hostname)
-  if(istat.eq.0) then
-     chead(1)(56:70)='0000'
+  if (istat .eq. 0) then
+     chead(1)(56:70) = '0000'
   else
-     chead(1)(56:70)='0001'
+     chead(1)(56:70) = '0001'
   endif
 #else
-  chead(1)(56:70)=hostnam(hostname)
+  chead(1)(56:70) = hostnam(hostname)
 #endif
-  chead(1)(72:80)='grb2index'
-  chead(1)(81:81)=char(10)
+  chead(1)(72:80) = 'grb2index'
+  chead(1)(81:81) = char(10)
 
   !  FILL SECOND 81-BYTE HEADER
-  chead(2)='IX1FORM:'
-  write(chead(2)(9:38),'(3i10)') 162,nlen,nnum
-  chead(2)(41:80)=cgb(ncgb1:ncgb)
-  chead(2)(81:81)=char(10)
+  chead(2) = 'IX1FORM:'
+  write(chead(2)(9:38),'(3i10)') 162, nlen, nnum
+  chead(2)(41:80) = cgb(ncgb1:ncgb)
+  chead(2)(81:81) = char(10)
 
   !  write headers at beginning of index file
-  call bawrite(lugi,0,162,kw,chead)
+  call bawrite(lugi, 0, 162, kw, chead)
 
   return
 end subroutine wrgi1h
@@ -167,14 +171,17 @@ end subroutine wrgi1h
 !> @return The index of the basename within the string.
 !>
 !> @author Iredell @date 93-11-22
-function ncbase(c,n)
+function ncbase(c, n)
+  implicit none
   character c*(*)
+  integer :: n
+  integer :: k, ncbase
 
-  k=n
-  do while(k.ge.1.and.c(k:k).ne.'/')
-     k=k-1
+  k = n
+  do while (k .ge. 1 .and. c(k:k) .ne. '/')
+     k = k - 1
   enddo
-  ncbase=k+1
+  ncbase = k + 1
 
   return
 end function ncbase
