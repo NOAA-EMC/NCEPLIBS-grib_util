3d477f07c045326cb6459e0e5de1aa3cd070d2df
Edward Hartnett <edward.hartnett@noaa.gov>
2024-02-26 05:30:14 -0700

    fixed comments

--- tests/run_degrib2_large_file_tests.sh
+++ tests/run_degrib2_large_file_tests.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # This is a test script for the NCEPLIBS-grib_util project.
 #
-# This script runs degrib2 on a extra very large GRIB2 data files
+# This script runs degrib2 on a very large GRIB2 data file
 # downloaded from the NOAA EMC FTP site, and checks the output against
 # reference output. This test is only run of the Cmake option
-# FTP_EXTRA_TEST_FILES is ON.
+# FTP_LARGE_TEST_FILES is ON.
 #
-# Ed Hartnett, 2/21/24
+# Ed Hartnett, 2/26/24
 
 set -e
 echo ""
-echo "*** Running degrib2 FTP extra file tests"
+echo "*** Running degrib2 FTP large file tests"
 
 ftp_files="fv3lam.t00z.prslev.f000.grib2"
 
