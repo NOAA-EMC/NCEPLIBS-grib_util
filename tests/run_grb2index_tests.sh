35eb223894f3e44028697aac8bc69c515a52c131
Edward Hartnett <edward.hartnett@noaa.gov>
2024-03-20 19:31:35 -0600

    more testing

--- tests/run_grb2index_tests.sh
+++ tests/run_grb2index_tests.sh
@@ -29,5 +29,12 @@ echo "*** Running grb2index tests"
 # so ignore them.
 cmp -i 140 test_gdaswave.grb2index.idx data/ref_gdaswave.grb2index.idx
 
+# Create an index of a GRIB2 file.
+../src/grb2index/grb2index 2 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.grb2index.idx2
+
+# Check against expected output. First 120 bytes contain differences,
+# so ignore them.
+cmp -i 140 test_gdaswave.grb2index.idx2 data/ref_gdaswave.grb2index.idx2
+
 echo "*** SUCCESS!"
 exit 0
