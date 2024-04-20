96204215d58130f34fa6d6c53deab616fda0b32b
Edward Hartnett <edward.hartnett@noaa.gov>
2024-04-20 07:00:53 -0600

    adding test

---
+++ tests/run_tocgrib2_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is a test script for the NCEPLIBS-grib_util project. This tests
+# the tocgrib2 utility.
+#
+# Ed Hartnett, 4/20/24
+
+set -e
+echo ""
+echo "*** Running tocgrib2 test"
+
+# Convert test file to GRIB1.
+#../src/tocgrib2/tocgrib2  data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000.grib1
+
+
+echo "*** SUCCESS!"
+exit 0
