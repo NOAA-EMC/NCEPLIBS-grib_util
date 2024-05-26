16831988071599818e5126543641b28f57df53e1
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-26 06:42:49 -0600

    working on tocgrib2 testing

--- tests/run_tocgrib2_tests.sh
+++ tests/run_tocgrib2_tests.sh
@@ -8,15 +8,15 @@ set -e
 echo ""
 echo "*** Running tocgrib2 tests"
 
-echo "*** Running tocgrib2 without input/output defined. Should return STOP 10"
-unset FORT11
-unset FORT51
-../src/tocgrib2/tocgrib2 && exit 1
+# echo "*** Running tocgrib2 without input/output defined. Should return STOP 10"
+# unset FORT11
+# unset FORT51
+# ../src/tocgrib2/tocgrib2 && exit 1
 
 echo "*** Running tocgrib2 with input/output defined."
-export FORT11="data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2"
+export FORT11="data/rrfs.t12z.prslevfaa.f010.na3km.grib2"
 export FORT51=out.grib2
-../src/tocgrib2/tocgrib2
+../src/tocgrib2/tocgrib2 < data/tocgrib2.nml
 
 echo "*** SUCCESS!"
 exit 0
