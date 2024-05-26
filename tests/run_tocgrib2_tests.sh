08e3ad4c8986a6136a4f39f5fa00f9fea6e16302
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-26 06:50:17 -0600

    more work on tocgrib2 testing

--- tests/run_tocgrib2_tests.sh
+++ tests/run_tocgrib2_tests.sh
@@ -13,7 +13,12 @@ echo "*** Running tocgrib2 tests"
 # unset FORT51
 # ../src/tocgrib2/tocgrib2 && exit 1
 
-echo "*** Running tocgrib2 with input/output defined."
+echo "*** Running tocgrib2 with input/output defined, but one missing field. It will fail."
+export FORT11="data/rrfs.t12z.prslevfaa.f010.na3km.grib2"
+export FORT51=out.grib2
+../src/tocgrib2/tocgrib2 < data/tocgrib2_bad.nml && exit 1
+
+echo "*** Running tocgrib2 with input/output defined. It will succeed."
 export FORT11="data/rrfs.t12z.prslevfaa.f010.na3km.grib2"
 export FORT51=out.grib2
 ../src/tocgrib2/tocgrib2 < data/tocgrib2.nml
