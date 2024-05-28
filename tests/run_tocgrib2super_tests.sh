c64324d1c2ea3606dee14726d97147686db775c6
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-28 11:33:55 -0600

    adding tests for tocgrib2super

--- tests/run_tocgrib2super_tests.sh
+++ tests/run_tocgrib2super_tests.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 # This is a test script for the NCEPLIBS-grib_util project. This tests
-# the tocgrib2 utility.
+# the tocgrib2super utility.
 #
-# Ed Hartnett, 4/20/24
+# Ed Hartnett, 5/28/24
 
 set -e
 echo ""
-echo "*** Running tocgrib2 tests"
+echo "*** Running tocgrib2super tests"
 
 echo "*** Running tocgrib2 without input/output defined. Should return STOP 10"
 unset FORT11
 unset FORT51
-../src/tocgrib2/tocgrib2 && exit 1
+../src/tocgrib2super/tocgrib2super && exit 1
 
-echo "*** Running tocgrib2 with input/output defined, but one missing field. It will fail."
+echo "*** Running tocgrib2super with input/output defined, but one missing field. It will fail."
 export FORT11="data/rrfs.t12z.prslevfaa.f010.na3km.grib2"
 export FORT51=out.grib2
-../src/tocgrib2/tocgrib2 < data/tocgrib2_bad.nml && exit 1
+../src/tocgrib2super/tocgrib2super < data/tocgrib2_bad.nml && exit 1
 
 echo "*** Running tocgrib2 with input/output defined. It will succeed."
 export FORT11="data/rrfs.t12z.prslevfaa.f010.na3km.grib2"
 export FORT51=out.grib2
-../src/tocgrib2/tocgrib2 < data/tocgrib2.nml
+../src/tocgrib2super/tocgrib2super < data/tocgrib2.nml
 
 echo "*** SUCCESS!"
 exit 0
