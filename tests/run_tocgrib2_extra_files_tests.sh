81e0ff3e0e949838074a86b899e1b9e951bf373d
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-28 12:25:01 -0600

    more testing

---
+++ tests/run_tocgrib2_extra_files_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# This is a test script for the NCEPLIBS-grib_util project. This tests
+# the tocgrib2 utility.
+#
+# Ed Hartnett, 4/20/24
+
+set -e
+echo ""
+echo "*** Running tocgrib2 extra file tests"
+
+echo "*** Running tocgrib2 with input/output defined, but one missing field. It will fail."
+export FORT11="data/rrfs.t12z.prslevfaa.f010.na3km.grib2"
+export FORT51=out.grib2
+../src/tocgrib2/tocgrib2 < data/tocgrib2_bad.nml && exit 1
+
+echo "*** Running tocgrib2 with input/output defined. It will succeed."
+export FORT11="data/rrfs.t12z.prslevfaa.f010.na3km.grib2"
+export FORT51=out.grib2
+../src/tocgrib2/tocgrib2 < data/tocgrib2.nml
+
+echo "*** SUCCESS!"
+exit 0
