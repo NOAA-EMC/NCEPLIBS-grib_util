679cd11d1078dc39ff4b6f9d6ee1eddb01b9ff62
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-28 10:14:15 -0600

    working on test

--- tests/run_copygb2_tests2.sh
+++ tests/run_copygb2_tests2.sh
@@ -21,10 +21,10 @@ echo "*** Running copygb2 test"
 diff -w test_gdaswave_2.grib2.degrib2 data/ref_copygb2_test_gdaswave.degrib2.txt
 
 # Invoke interpolation logic.
-#../src/copygb2/copygb2 -g "30 6 0 0 0 0 0 0 1473 1025 12190000 226541000 8 25000000 265000000 5079000 5079000 0 64 25000000 25000000" -i"1 1" -x data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave_2.ip.grib2
+../src/copygb2/copygb2 -g "30 6 0 0 0 0 0 0 1473 1025 12190000 226541000 8 25000000 265000000 5079000 5079000 0 64 25000000 25000000" -i"1 1" -x data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave_2.ip.grib2
 
 # Make degrib2 output for the copied file.
-#../src/degrib2/degrib2 test_gdaswave_2.ip.grib2 > test_gdaswave_2.ip.grib2.degrib2
+../src/degrib2/degrib2 test_gdaswave_2.ip.grib2 > test_gdaswave_2.ip.grib2.degrib2
 
 # Check against expected output.
 #diff -w test_gdaswave_2.ip.grib2.degrib2 data/ref_copygb2_test_gdaswave_2.ip.grib2.degrib2
